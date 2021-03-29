e8f175af55ebd3180b6bec4168901aefbff6de27
Jackson McCrory <jackson@mccrory.xyz>
2021-03-28 20:26:06 -0700

    Fixed a permission issue

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ git clone https://github.com/Se7endAY/grub2-theme-vimix Vimix
 sudo mv Vimix /usr/share/grub/themes/
 
 grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sudo sed -i '/GRUB_THEME=/d' /etc/default/grub
-echo "GRUB_THEME=\"/usr/share/grub/themes/Vimix/theme.txt\"" >> /etc/default/grub
+sudo echo "GRUB_THEME=\"/usr/share/grub/themes/Vimix/theme.txt\"" >> /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
