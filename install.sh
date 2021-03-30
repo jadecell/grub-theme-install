9432ec02f53626ac39cfaa7efdb28856e7f09054
Jackson McCrory <jackson@mccrory.xyz>
2021-03-29 22:11:23 -0700

    fixed permission

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 
+[ "$(id -u)" -ne 0 ] && echo "Run as root" && exit 1
 # Make backup
-sudo cp -an /etc/default/grub /etc/default/grub.bak
-
-sudo mkdir -p /usr/share/grub/themes/
+cp -an /etc/default/grub /etc/default/grub.bak
+mkdir -p /usr/share/grub/themes/
 
 git clone https://github.com/Se7endAY/grub2-theme-vimix Vimix
-sudo mv Vimix /usr/share/grub/themes/
+mv Vimix /usr/share/grub/themes/
 
 grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sudo sed -i '/GRUB_THEME=/d' /etc/default/grub
-sudo echo "GRUB_THEME=\"/usr/share/grub/themes/Vimix/theme.txt\"" >> /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+echo "GRUB_THEME=\"/usr/share/grub/themes/Vimix/theme.txt\"" >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
