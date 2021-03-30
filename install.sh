b6acbc662c19c9b8206a0a4267a4c16bff933242
Jackson McCrory <jackson@mccrory.xyz>
2021-03-29 22:16:58 -0700

    fixed the theme issue

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp -an /etc/default/grub /etc/default/grub.bak
 mkdir -p /usr/share/grub/themes/
 
 git clone https://github.com/Se7endAY/grub2-theme-vimix Vimix
-mv Vimix /usr/share/grub/themes/
+mv Vimix/Vimix /usr/share/grub/themes/
 
 grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sudo sed -i '/GRUB_THEME=/d' /etc/default/grub
 echo "GRUB_THEME=\"/usr/share/grub/themes/Vimix/theme.txt\"" >> /etc/default/grub
